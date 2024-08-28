86daf13a49225229e30a9cb2c27fc5e9634ade93
Jan Romel <janjancaranguian@gmail.com>
2024-08-28 09:57:10 +0800

    Make the movement animation functional

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @export var move_speed: float = 100
 
+
+
+
 func _physics_process(_delta):
 	#Get input direction
 	var input_direction = Vector2 (
@@ -12,5 +15,14 @@ func _physics_process(_delta):
 	# Update velocity
 	velocity = input_direction * move_speed
 	
-	# Move and Slice function uses velocity of character body to move character on map
-	move_and_slide()
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("Idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("Walk")
+		$AnimationTree.set("parameters/Idle/blend_position", velocity)
+		$AnimationTree.set("parameters/Walk/blend_position", velocity)
+		# Move and Slice function uses velocity of character body to move character on map
+		move_and_slide()
+	
+	
+	
